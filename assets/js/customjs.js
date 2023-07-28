8367f407ae2483504d00adbd803bc4d8989d0975
devsaket <dmsaketk@gmail.com>
2023-07-28 12:42:04 +0530

    dev

---
+++ assets/js/customjs.js
@@ -0,0 +1,12 @@
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 3000,
+  touch: false,
+  hover: false,
+  pause: false,
+})
+
+
+
+
