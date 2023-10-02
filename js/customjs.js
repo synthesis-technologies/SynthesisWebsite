76904104c1ec617a34938d29f557daf9d0cd209d
devsaket <dmsaketk@gmail.com>
2023-10-02 11:50:08 +0530

    updated_27_09

---
+++ js/customjs.js
@@ -0,0 +1,46 @@
+// const myCarouselElement = document.querySelector('#myCarousel')
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 3000,
+//   touch: false,
+//   hover: false,
+//   pause: false,
+// })
+
+$(document).ready(function () {
+  $('.dropdown').hover(function () {
+    $(this).addClass('show');
+    $(this).find('.dropdown-menu').addClass('show');
+  }, function () {
+    $(this).removeClass('show');
+    $(this).find('.dropdown-menu').removeClass('show');
+  });
+
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay:true,
+    dots: true,
+    autoplayHoverPause: true,
+    mergeFit: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      992: {
+        items: 3
+      }
+    }
+  })
+
+  var owl = $(".owl-carousel");
+	owl.owlCarousel();
+	$(".next-btn").click(function () {
+		owl.trigger("next.owl.carousel");
+	});
+	$(".prev-btn").click(function () {
+		owl.trigger("prev.owl.carousel");
+	});
+
+});
