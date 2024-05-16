2cbbc1bcafc5f6c55c9fd8526fd0d089b733df43
Vimalranaa <royalrana34@gmail.com>
2024-05-16 17:39:01 +0530

    portfolio and contact form working done

--- js/customjs.js
+++ js/customjs.js
@@ -44,3 +44,15 @@ $(document).ready(function () {
 	});
 
 });
+
+
+//<=====FAQ=====>
+  jQuery(document).ready(function(){
+    jQuery('.titleWrapper').click(function(){
+      var toggle = jQuery(this).next('div#descwrapper');
+      jQuery(toggle).slideToggle("slow");
+    });
+      jQuery('.inactive').click(function(){
+      jQuery(this).toggleClass('inactive active');
+    });
+  });
